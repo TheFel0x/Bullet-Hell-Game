4c8aa9d41e9ed5611bdba428c4b985ea5971b680
TheFel0x <dev.thefel0x@gmail.com>
2023-10-14 17:44:31 +0200

    Player Bullets don't move yet

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,11 +5,22 @@ const SPEED = 400.0
 const PRECISION_SPEED = 200.0
 var velocity: Vector2 = Vector2(0, 0)
 
+# TODO: Maybe use some other type of bullet?
+const BulletScene = preload("res://scenes/bullet_2d.tscn") 
+
 func get_input():
 	var precision_mode: bool = Input.is_action_pressed("precision")
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction
 	velocity *= PRECISION_SPEED if precision_mode else SPEED
+	
+	# Handle Shooting input
+	if Input.is_action_just_pressed("shoot"):
+		# Initial shot has no delay ; FIXME: this potentially allows for spamming bullets
+		_on_shoot_timer_timeout()
+		$ShootTimer.start()
+	if Input.is_action_just_released("shoot"):
+		$ShootTimer.stop()
 
 func _physics_process(delta):
 	get_input()
@@ -29,3 +40,21 @@ func _on_area_entered(area):
 		for group in area.get_groups():
 			all_groups += group + " "
 		print_debug("Not sure what it is: ["+all_groups+"]")
+
+func _on_shoot_timer_timeout():
+	# Spawn Bullet
+	var direction: Vector2 = Vector2(0,-1000)
+	var emission_distance: int = 20
+	
+	var bullet_inst: Bullet2D = BulletScene.instantiate()
+	
+	bullet_inst.position = global_position + direction.normalized() * emission_distance
+	# FIXME: use of magic string
+	bullet_inst.set_animation("round")
+	#bullet_inst.set_direction(Vector2(0,0))
+	#bullet_inst.look_at()
+	get_parent().get_parent().add_child(bullet_inst)
+	
+	print_debug("Pew!")
+	
+
